be8b0815fbc0f3f6ec8192d0d8cbb6b08c67b9eb
virtyalochka <valovalexcen@mail.ru>
2023-11-15 22:03:10 +0300

    added working docker

--- app/config.py
+++ app/config.py
@@ -2,10 +2,10 @@ import os
 
 SECRET_KEY = '861b1dabae5b0c120887061e55b958a4acbd2cd5850b66412f6e38cec69b0e93'
 
-MYSQL_USER = 'std_1975_exam'
-MYSQL_PASSWORD = 'qwerty12'
-MYSQL_HOST = 'std-mysql.ist.mospolytech.ru'
-MYSQL_DATABASE = 'std_1975_exam'
+MYSQL_USER = 'root'
+MYSQL_PASSWORD = 'Qwerty_123'
+MYSQL_HOST = 'mysql'
+MYSQL_DATABASE = 'sys'
 
 ADMIN_ROLE_ID = 1
 MODER_ROLE_ID = 2
