8a28a51944fe948b02dc32e1bbb5962a976374d0
virtyalochka <valovalexcen@mail.ru>
2023-12-16 20:48:59 +0300

    start working with prometheus

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,3 @@
-# Используем базовый образ с поддержкой Java (OpenJDK)
-FROM openjdk:11-jre-slim
-
 # Используем базовый образ Python с поддержкой Flask
 FROM python:3.9
 
@@ -18,21 +15,5 @@ ENV FLASK_APP=app.py
 # Определяем порт, который будет слушать Flask
 EXPOSE 5000
 
-# Устанавливаем Flyway
-WORKDIR /flyway
-RUN apt-get update && \
-    apt-get install -y wait-for-it && \
-    wget -qO- https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/7.0.0/flyway-commandline-7.0.0-linux-x64.tar.gz | tar xvz && \
-    ln -s /flyway/flyway-7.0.0/flyway /usr/local/bin/flyway && \
-    apt-get remove -y wget && \
-    apt-get clean && \
-    rm -rf /var/lib/apt/lists/*
-
-# Возвращаемся в /app
-WORKDIR /app
-
 # Запускаем миграцию приложения
-CMD ["bash", "-c", "wait-for-it mysql:3306 -- flyway repair -url=jdbc:mysql://mysql:3306/sys -user=root -password=Qwerty_123 && flyway migrate -url=jdbc:mysql://mysql:3306/sys -user=root -password=Qwerty_123 -locations=filesystem:/app/sys && python -m flask run --host=0.0.0.0"]
-
-
-
+CMD ["bash", "-c", "python -m flask run --host=0.0.0.0"]
--- app/app.py
+++ app/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, send_from_directory
+from prometheus_client import Counter, generate_latest, REGISTRY
 from mysql_db import MySQL
 
 app = Flask(__name__)
+
 app.config.from_pyfile('config.py')
 
 db = MySQL(app)
@@ -13,12 +15,40 @@ from collect import bp as collections_bp
 app.register_blueprint(auth_bp)
 app.register_blueprint(books_bp)
 app.register_blueprint(collections_bp)
+
 init_login_manager(app)
+
+# Define Prometheus metrics
+http_requests_total = Counter('http_requests_total', 'Total number of HTTP requests')
+function_calls_total = Counter('function_calls_total', 'Total number of function calls with labels', labelnames=['endpoint'])
+errors_total = Counter('errors_total', 'Total number of errors with labels', labelnames=['endpoint'])
+
+def increment_function_calls(endpoint):
+    function_calls_total.labels(endpoint=endpoint).inc()
+
+def increment_errors(endpoint):
+    errors_total.labels(endpoint=endpoint).inc()
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    http_requests_total.inc()  # Increment the HTTP request counter
+    try:
+        # Your existing code for the index function
+
+        # Increment the function call counter for the index function
+        increment_function_calls('index')
+        return render_template('index.html')
+    except Exception as e:
+        # Increment the error counter for the index function
+        increment_errors('index')
+        raise e
 
 @app.route('/media/<path:filename>')
 def media(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename, mimetype='image/jpeg')
+
+# Endpoint for Prometheus to scrape metrics
+@app.route('/metrics')
+def metrics():
+    return generate_latest(REGISTRY), 200, {'Content-Type': 'text/plain'}
+
